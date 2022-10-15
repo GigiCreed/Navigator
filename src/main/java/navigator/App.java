ad04bd5dcc92077642114082d703e4d558b6e933
StarFleet1334 <86927329+StarFleet1334@users.noreply.github.com>
2022-10-16 00:04:12 +0400

    Commit

--- src/main/java/navigator/App.java
+++ src/main/java/navigator/App.java
@@ -17,6 +17,7 @@ public class App
     public static void main( String[] args )
     {
         Navigator navigator = new Navigator();
+        System.out.println("asd");
 
         navigator.showDistance("Batumi A","Batumi F");
     }
