899b54facdd9499ee9f1e6bd3efc4ff278580d44
GigiMamaladze <gigimamaladze13@gmail.com>
2022-10-17 22:20:37 +0400

    FINISHED

--- src/main/java/navigator/database/dao/interfaces/IRoadToStationDAO.java
+++ src/main/java/navigator/database/dao/interfaces/IRoadToStationDAO.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface IRoadToStationDAO extends IBaseDAO<RoadToStation>{
     List<RoadToStation> getAllByCitiesId(int cities_id);
 
-    List<RoadToStation> getAllForCityDistance();
+
 }
